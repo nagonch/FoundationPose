243f5a761dca7171c0463ee33471a721bcb840c8
Your Name <you@example.com>
2025-06-26 12:44:45 +1000

    change run container

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,20 @@
 docker rm -f foundationpose
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+xhost local:1000 + && \
+docker run \
+    --name foundationpose  \
+    --gpus all \
+    --env NVIDIA_DISABLE_REQUIRE=1 \
+    -it \
+    --cap-add=SYS_PTRACE \
+    --security-opt seccomp=unconfined \
+    -v $DIR:$DIR \
+    -v /home:/home \
+    -v /mnt:/mnt  \
+    -v /tmp:/tmp  \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/root/.Xauthority:rw \
+    --network=host \
+    --ipc=host \
+    -e DISPLAY=${DISPLAY} \
+    -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
